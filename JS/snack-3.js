f9b5a7311680bbe21fcc7784047e52d3ceabac02
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-09 16:55:35 +0200

    first commit

---
+++ JS/snack-3.js
@@ -0,0 +1,2 @@
+// Scrivi una funzione che accetti una stringa come argomento e la ritorni
+// girata (es. Ciao -> oaiC)
---
+++ JS/snack-4.js
@@ -0,0 +1,3 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di
+//     elementi) prendendo alternativamente gli elementi da uno e dall’altro
+//     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
