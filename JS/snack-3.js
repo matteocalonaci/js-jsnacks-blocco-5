e7a245318bc52b0c527a3d98df6da0d497571116
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-09 18:29:00 +0200

    SNACK 3

--- JS/snack-3.js
+++ JS/snack-3.js
@@ -1,2 +1,28 @@
 // Scrivi una funzione che accetti una stringa come argomento e la ritorni
 // girata (es. Ciao -> oaiC)
+
+
+//FUNZIONE ANONIMA GIRATA
+// const parola = 'ciao';
+
+
+// function reverse() {
+//     let parolaDivisa = parola.split('');
+//     parolaGirata = parolaDivisa.reverse();
+//     console.log('ti ho girata maledetta', parolaGirata);
+
+// }
+// reverse()
+
+
+//FUNZIONE CON ARGOMENTO GIRATA
+
+function girata(element) {
+    let parola = element
+    let parolaDivisa = parola.split('');
+    let parolaRovesciata = parolaDivisa.reverse();
+    return parolaRovesciata
+}
+
+let parolaRovesciata = girata("Genoveffo")
+console.log('ti ho girata maledetta', parolaRovesciata);
