2bd6db79bee335184f44cc3fb4954a8d48862e72
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-09 18:49:27 +0200

    SNACK 4

--- JS/snack-4.js
+++ JS/snack-4.js
@@ -1,3 +1,19 @@
 // Scrivi una funzione che fonda due array (aventi lo stesso numero di
 //     elementi) prendendo alternativamente gli elementi da uno e dall’altro
 //     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+console.log("ciao")
+
+let arrayUno = [1, 2, 3]
+let arrayDue = ["a", "b", "c"]
+let arrayTre = []
+function pusha() {
+    for (let i = 0; i < arrayUno.length && arrayDue.length; i++) {
+        let elementiUno = arrayUno[i]
+        let elementiDue = arrayDue[i]
+        arrayTre.push(elementiUno, elementiDue)
+        console.log(arrayTre)
+    }
+}
+
+pusha()
+
