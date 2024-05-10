e0d30da511b288bd6d41d2109e3fdddba3f86fa7
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-10 12:22:22 +0200

    Aggiunte e miglioramenti

--- JS/snack-3.js
+++ JS/snack-3.js
@@ -26,3 +26,43 @@ function girata(element) {
 
 let parolaRovesciata = girata("Genoveffo")
 console.log('ti ho girata maledetta', parolaRovesciata);
+
+
+// VERSIONE CON CICLO CORRETTO IN CLASSE
+
+let parola = "ciao"
+let ultimoCarattere = parola.length - 1
+parolaAlContrario = "";
+
+for (let i = ultimoCarattere; i >= 0; i--) {
+    let elmetto = parolaa[i]
+    console.log(elmetto)
+
+    parolaAlContrario += elmetto
+}
+console.log(parolaAlContrario)
+
+// E SE IL CICLO NON FOSSE -- MA ++ ?
+
+let reverseWord = '';
+
+for (let i = 0; i < word.length; i++) {
+
+    reverseWord = word.charAt(i) + reverseWord;
+};
+//ad ogni giro di ciclo prendi il carattere i a cui vado add il testo in reverseWord
+//    c+ null --> c
+//    i+c --> ic
+//    a+ic --> aic
+//    o+aic --> oaic
+
+for (let i = 0; i < String.length; i++) {
+    output += String[String.length - i - 1]
+};
+
+// 4 - 0 --> 4 (o)
+// 4 - 1 --> 3 (a)
+// 4 - 2 --> 2 (i)
+// 4 - 3 --> 1 (c)
+
+
--- JS/snack-4.js
+++ JS/snack-4.js
@@ -17,3 +17,86 @@ function pusha() {
 
 pusha()
 
+//NON VA
+
+// let array1 = [1, 2, 3]
+// let array2 = ["a", "b", "c"]
+// let array3 = []
+
+// function joint(array1, array2, array3) {
+//     let i = 0
+//     while (i < array1.length && i < array2.length) {
+//         array3.push(array1[i])
+//         array3.push(array2[i])
+//         i++
+//     }
+
+//     if (array1.length > array2.length) {
+//         while (i < array1.length) {
+//             array3.push(array1[i])
+//             i++
+//         }
+//     }
+//     else if (array2.length > array1.length) {
+//         while (i < array2.length) {
+//             array3.push(array2[i])
+//             i++
+//         }
+//     }
+//     console.log("altra soluzione", array3)
+// }
+
+// joint()
+
+//SE GLI ARRY HANNO DIVERSE LUNGHEZZE?
+const primo = ["a", "b", "c", "d", "e"]
+const secondo = [1, 2, 3, 4, 5, 6, 7, 8]
+
+const risultato = []
+//di base consideriamo primo > secondo
+let limiteCiclo = primo.length
+
+//se fosse viceversa il valre di limiteCiclo sarà secondo.length
+if (primo.length > secondo.length) {
+    let limiteCiclo = secondo.length
+}
+
+//una volta constatato quale sia limiteCiclo
+// iniziamo un ciclo
+
+for (let i = 0; i < limiteCiclo; i++) {
+    console.log(primo[i])
+    console.log(secondo[i])
+
+    risultato.push(primo[i], secondo[i])
+
+}
+console.log("sono il risultato", risultato)
+
+
+//FUNZIONE COM 2 ARGOMENTI
+// ARRAY1 E ARRAY2 IN ARRAY3
+
+function mischia(primo, secondo) {
+    const risultato = []
+    if (primo.length > secondo.length) {
+        let limiteCiclo = secondo.length
+    }
+
+    //una volta constatato quale sia limiteCiclo
+    // iniziamo un ciclo
+
+    for (let i = 0; i < limiteCiclo; i++) {
+        console.log(primo[i])
+        console.log(secondo[i])
+
+        risultato.push(primo[i], secondo[i])
+
+    }
+    // console.log("sono il risultato", risultato)
+    return risultato
+
+}
+
+let arrayPazzo = mischia(["a", "b", "c", "d", "e"], [1, 2, 3, 4, 5, 6, 7, 8])
+console.log("perchè io valgo:", arrayPazzo)
